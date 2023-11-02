c2b304160f39d6de41e32639a5408c6594effe58
Michelangelo Marani <michelangelomarani88@gmail.com>
2023-11-02 10:36:41 +0100

    feat: add navabar to not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,24 @@
+// libraries
 import { Link } from "react-router-dom";
+
+// components
+import Header from "./Header";
 
 const NotFound = () => {
 	return (
-		<section className="page">
-			<h1>Not found</h1>
-			<p>
-				We can't find the Resource you looked for, please navigate back to the
-				Home.
-			</p>
-			<Link className="btn" to="/">
-				Back to the Home
-			</Link>
-		</section>
+		<div>
+			<Header />
+			<section className="page not-found">
+				<h1>Not found</h1>
+				<p>
+					We can't find the Resource you looked for, please navigate back to the
+					Home.
+				</p>
+				<Link className="btn" to="/">
+					Back to the Home
+				</Link>
+			</section>
+		</div>
 	);
 };
 
